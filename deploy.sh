917764ab0548d4859309049f4e78c27db56af841
Chemaclass <chemaclass@outlook.es>
2025-04-15 20:03:10 +0200

    split deploy and install script

--- deploy.sh
+++ deploy.sh
@@ -12,21 +12,6 @@ echo "📥 Pulling latest changes from Git..."
 git checkout $BRANCH
 git pull origin $BRANCH
 
-echo "🎼 Running composer install..."
-composer install --no-interaction --prefer-dist --optimize-autoloader
-
-echo "📦 Installing npm dependencies..."
-npm install
-
-echo "🛠 Building frontend assets..."
-npm run build
-
-echo "🧹 Clearing and caching Laravel config..."
-php artisan config:clear
-php artisan config:cache
-php artisan route:clear
-php artisan route:cache
-php artisan view:clear
-php artisan view:cache
+./install.sh
 
 echo "✅ Deployment finished!"
