7d1be438ffa5a7942313f19a12748c27211abfda
Chemaclass <chemaclass@outlook.es>
2025-04-27 22:13:52 +0200

    fix manifestDir

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     base: '/build/',
     build: {
         manifest: true,
+        manifestDir: '.',
         outDir: 'public/build',
         assetsDir: 'assets',
         rollupOptions: {
