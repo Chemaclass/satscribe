4744697380dd42e368b4b53db3a2eb0861de0bfd
Chemaclass <chemaclass@outlook.es>
2025-04-27 22:02:30 +0200

    fix: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import {defineConfig} from 'vite';
-
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/build/',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        assetsDir: 'assets',
+        rollupOptions: {
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
