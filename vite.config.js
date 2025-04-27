bd703734907c5a08647b5f837d699aa12ca20d49
Chemaclass <chemaclass@outlook.es>
2025-04-27 21:55:17 +0200

    add build assets to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/build/',
+    build: {
+        outDir: 'public/build',
+        assetsDir: 'assets',
+        manifest: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
