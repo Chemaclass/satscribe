5f8838a1f4b839bd3a3375c970eab6d09cd1a8f6
Chemaclass <chemaclass@outlook.es>
2025-05-06 10:37:11 +0200

    improve home header on phone

--- resources/views/components/home/header.blade.php
+++ resources/views/components/home/header.blade.php
@@ -4,7 +4,7 @@
             Explore Any Bitcoin Transaction or Block
         </h1>
         <p class="subtitle text-base sm:text-lg text-gray-700 leading-relaxed">
-            <strong>Satscribe</strong> makes Bitcoin easier to understand. <br>
+            <strong>Satscribe</strong> makes Bitcoin easier to understand. <br class="hidden sm:block">
             Just enter a transaction ID or block height to get an AI-powered explanation.
         </p>
         <p class="subtitle mt-1 text-sm text-gray-600 leading-relaxed">
