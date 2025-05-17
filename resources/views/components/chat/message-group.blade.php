f34f05811b3a4c14ddf38ded618415890548696a
Chemaclass <chemaclass@outlook.es>
2025-05-17 23:25:24 +0200

    chore: add icons to You and assistant in chat

--- resources/views/components/chat/message-group.blade.php
+++ resources/views/components/chat/message-group.blade.php
@@ -6,16 +6,23 @@
 <div class="chat-message-group mb-6">
     @if($userMsg)
         <div class="user-message mb-2 text-right">
-            <span class="font-semibold">You:</span>
-            <div class="inline-block rounded px-3 py-2">
-                {{ $userMsg->content }}
+            <div class="flex items-center gap-1 justify-end">
+                <i data-lucide="user" class="w-4 h-4"></i>
+                <span class="font-semibold">You:</span>
+                <div class="inline-block rounded px-3 py-2">
+                    {{ $userMsg->content }}
+                </div>
             </div>
         </div>
     @endif
 
     @if($assistantMsg)
         <div class="assistant-message text-left">
-            <span class="font-semibold">Scribe:</span>
+            <span class="font-semibold flex items-center gap-1">
+                <i data-lucide="bot" class="w-4 h-4"></i>
+                Scribe:
+            </span>
+
             <div class="inline-block rounded px-3 py-2">
                 {!! Str::markdown($assistantMsg->content) !!}
             </div>
