37532bddcc6bb35a9fb2922ba402b000d8af5d58
Chemaclass <chemaclass@outlook.es>
2025-05-17 23:48:56 +0200

    chore: remove link on createdAt

--- resources/views/components/chat/raw-data-toggle-button.blade.php
+++ resources/views/components/chat/raw-data-toggle-button.blade.php
@@ -1,18 +1,7 @@
 @props(['input', 'question', 'createdAt', 'id'])
 
 <div class="chat-meta mt-4 flex justify-between items-center text-sm text-gray-500">
-    @if ($createdAt->diffInMinutes(now()) > 1)
-        <button
-            type="button"
-            class="text-sm text-gray-500 hover:text-orange-400 cursor-pointer"
-            onclick="resubmitWithRefresh('{{ $input }}', '{{ $question }}')"
-            title="Refresh this result with latest data"
-        >
-            {{ $createdAt->diffForHumans() }}
-        </button>
-    @else
-        <span>{{ $createdAt->diffForHumans() }}</span>
-    @endif
+    <span>{{ $createdAt->diffForHumans() }}</span>
 
     <button type="button"
             class="toggle-history-raw-btn link"
