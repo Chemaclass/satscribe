0a64a49ba5aa2a73a333393d790725c12bae1e63
Chemaclass <chemaclass@outlook.es>
2025-05-26 06:10:18 +0200

    refactor: SetLocale

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -1,13 +1,15 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class SetLocale
+final class SetLocale
 {
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $locale = $request->get('lang');
         if ($locale) {
@@ -15,7 +17,7 @@ class SetLocale
         }
         $locale = session('app_locale', config('app.locale'));
         app()->setLocale($locale);
+
         return $next($request);
     }
 }
-
