093042b22e8d8ccfd01de752ef751c376c9aefdc
Chemaclass <chemaclass@outlook.es>
2025-06-19 00:10:04 +0200

    hide detailed errors on prod

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,13 +6,29 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Throwable;
 
 final class Handler extends ExceptionHandler
 {
     public function render($request, Throwable $e): Response
     {
+        if (app()->environment('production')) {
+            // Custom response for method not allowed
+            if ($e instanceof MethodNotAllowedHttpException) {
+                Log::warning('Invalid HTTP method used', [
+                    'method' => $request->method(),
+                    'url' => $request->fullUrl(),
+                ]);
+
+                return response()->json([
+                    'message' => 'Method Not Allowed.',
+                ], Response::HTTP_METHOD_NOT_ALLOWED);;
+            }
+        }
+
         if ($e instanceof ThrottleRequestsException) {
             return response()->json([
                 'message' => $e->getMessage(),
