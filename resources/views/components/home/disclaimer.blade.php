9bd843bb3f651c7e3f33b1ce0e703371b5f669d4
Chemaclass <chemaclass@outlook.es>
2025-06-23 10:23:14 +0200

    Make disclaimer text bold on home

--- resources/views/components/home/disclaimer.blade.php
+++ resources/views/components/home/disclaimer.blade.php
@@ -4,7 +4,7 @@
     x-cloak
     class="mb-4 p-3 rounded-md bg-yellow-100 border border-yellow-200 text-yellow-800 text-sm flex justify-between items-start"
 >
-    <p>{{ __("Satscribe has nothing to do with Ordinals or inscriptions. It's an AI-powered tool designed to explain Bitcoin transactions and blocks in natural language.") }}</p>
+    <p>{!! __('home.disclaimer') !!}</p>
     <button
         type="button"
         class="ml-4 w-20 underline"
