7b028acf9aa68bb9782a6e9138aba65919b61ad6
Chemaclass <chemaclass@outlook.es>
2025-06-29 17:47:40 +0200

    fix: i18n the default title

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -16,7 +16,7 @@
 >
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'Satscribe – Satoshi Describer')</title>
+    <title>@yield('title', __('Unlock the Story Behind Every Satoshi') . ' – Satscribe')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="nostr-pubkey" content="{{ nostr_pubkey() }}">
