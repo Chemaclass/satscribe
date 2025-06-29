fd1b570f9824a16f41a784ff8af6573b56b1b5d6
Chemaclass <chemaclass@outlook.es>
2025-06-29 23:25:12 +0200

    refactor: remove unnecesary client_uuid() helper

--- modules/Shared/Infrastructure/helpers.php
+++ modules/Shared/Infrastructure/helpers.php
@@ -15,18 +15,10 @@ if (!\function_exists('client_ip')) {
     }
 }
 
-if (!\function_exists('client_uuid')) {
-    /** @todo not implemented yet */
-    function client_uuid(): string
-    {
-        return (string) Request::header('X-Client-UUID', '');
-    }
-}
-
 if (!\function_exists('tracking_id')) {
     function tracking_id(): string
     {
-        return nostr_pubkey() ?: hash('sha256', client_ip() . client_uuid() . Request::userAgent());
+        return nostr_pubkey() ?: hash('sha256', client_ip() . Request::userAgent());
     }
 }
 
