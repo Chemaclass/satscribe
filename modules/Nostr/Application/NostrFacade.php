415b43ead43c32a6b11fdd646c09318a5abcf7d9
Chemaclass <chemaclass@outlook.es>
2025-07-14 21:29:20 +0200

    chore: prepare NostrFacade

---
+++ modules/Nostr/Application/NostrFacade.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\NostrAuth\Application;
+
+use Modules\NostrAuth\Domain\NostrFacadeInterface;
+
+final readonly class NostrFacade implements NostrFacadeInterface
+{
+}
---
+++ modules/Nostr/Domain/NostrFacadeInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\NostrAuth\Domain;
+
+interface NostrFacadeInterface
+{
+}
